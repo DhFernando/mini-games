cb6744869be18fe409cd45f3df48b9e74f721f09
dhfernando <hdilshan.fernando11@gmail.com>
2024-08-31 22:34:04 +0530

    circular motion

--- main.js
+++ main.js
@@ -4,45 +4,26 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
 var c = canvas.getContext('2d')
+const colors  = [
+    '#ADD8E6', // Light Blue
+    '#87CEEB', // Sky Blue
+    '#4682B4', // Steel Blue
+    '#5F9EA0', // Cadet Blue
+    '#6495ED', // Cornflower Blue
+    '#4169E1', // Royal Blue
+    '#1E90FF', // Dodger Blue
+    '#0000FF', // Pure Blue
+    '#00008B', // Dark Blue
+    '#000080', // Navy Blue
+    '#191970', // Midnight Blue
+];
 
-// c.fillStyle = 'rgba(255, 0, 0, 0.5)'
-// c.fillRect(100, 100, 100, 100)
-// c.fillStyle = 'rgba(0, 255, 0, 0.5)'
-// c.fillRect(400, 100, 100, 100)
-// c.fillStyle = 'rgba(0, 0, 255, 0.5)'
-// c.fillRect(300, 300, 100, 100)
-
-// // Line
-// c.beginPath()
-// c.moveTo(60, 70)
-// c.lineTo(200, 100)
-// c.lineTo(400, 300)
-// c.lineTo(300, 50)
-// c.strokeStyle = "#fa34a3"
-// c.stroke()
-
-// // Arc / Circle
-// c.beginPath()
-// c.arc(300, 300, 30, 0, Math.PI * 2, false)
-// c.strokeStyle = 'black'
-// c.stroke()
-
-const colors = [
-    '#FF5733', // Vibrant Orange
-    '#33FF57', // Lime Green
-    '#3357FF', // Bright Blue
-    '#FF33A1', // Hot Pink
-    '#33FFF3', // Aqua Blue
-    '#FF5733', // Tangerine
-    '#5733FF', // Purple
-    '#F3FF33', // Yellow
-    '#FF3357', // Coral Red
-    '#33FFA1'  // Mint Green
-  ];
 
 
 const randomIntFromRange = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min)
+    const val = Math.random() * (max - min) + min
+    console.log(val)
+    return  val
 }
 
 const randomColor = () => {
@@ -62,105 +43,60 @@ const calculateDistance = (x1, y1, x2, y2) => {
     return Math.sqrt(dx * dx + dy * dy)
 }
 
+const mouseCoords = {
+    x: canvas.width/2,
+    y: canvas.height/2
+}
 
-class Enemy{
-    constructor(x, y, dX, dY, radius, color){
-        this.x = x
-        this.y = y
-        this.dX = dX
-        this.dY = dY
+window.addEventListener('mousemove', (event) => { 
+    mouseCoords.x = event.clientX
+    mouseCoords.y = event.clientY
+})
+
+class Particle{
+    constructor(radius, color, distance, velocity){
+        this.x = undefined 
+        this.y = undefined 
         this.radius = radius
-        this.color = color;
-        this.defaultRadius = radius;
-        this.defaultColor = color;
+        this.color = color;  
+        this.radians = Math.random() * Math.PI * 2
+        this.velocity = velocity
+        this.distance = distance
     } 
     draw(){
         c.beginPath()
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
         c.fillStyle = this.color
-        c.stroke()
-        c.fill()
-        c.closePath()
-    }
-
-    // interactivity(){
-    //     if(Math.abs(this.x - mouseCoords.x ) < this.radius + 50 && Math.abs(this.y - mouseCoords.y) < this.radius + 50){
-    //         if(this.radius < 50){
-    //             this.radius += 1
-    //         }
-    //     }else{
-    //         if(this.radius !== this.defaultRadius){
-    //             this.radius = this.defaultRadius
-    //         }
-    //     }
-    // }
-
-    update(){
-        if(this.x + this.radius > innerWidth || this.x - this.radius < 0){
-            this.dX = -this.dX
-        }
-    
-        if(this.y + this.radius > innerHeight || this.y - this.radius < 0){
-            this.dY = -this.dY
-        }
-        
-    
-        this.x = this.x + this.dX
-        this.y = this.y + this.dY
-
-        let d = calculateDistance(this.x, this.y, mouseCoords.x, mouseCoords.y)
-
-        if(d < 40 + this.radius){
-            this.color = 'green'
-        }else{
-            this.color = this.defaultColor
-        
-        }
-        // this.interactivity()
-        this.draw()
-    } 
-}
-
-
-const mouseCoords = {
-    x: undefined,
-    y: undefined
-}
-
-window.addEventListener('mousemove', (event) => {
-    console.log(event)
-    mouseCoords.x = event.clientX
-    mouseCoords.y = event.clientY
-})
-class Player{ 
-    draw(){
-        c.beginPath()
-        c.arc(mouseCoords.x, mouseCoords.y, 40, 0, Math.PI * 2, false)
-        c.fillStyle = 'red'
-        c.stroke()
         c.fill()
         c.closePath()
     } 
     update(){ 
-        // this.interactivity()
+        this.radians = this.radians + this.velocity
+        this.x = mouseCoords.x + Math.cos(this.radians)*this.distance
+        this.y = mouseCoords.y + Math.sin(this.radians)*this.distance
         this.draw()
     } 
 }
- 
- 
-let enemy;
-let player;
+  
+let partcles = [];
 // Animation
-const animate = () => {
-    c.clearRect(0, 0, innerWidth, innerHeight)
-    requestAnimationFrame(animate) 
-    enemy.update()
-    player.update() 
+const animate = () => { 
+    c.fillStyle = 'rgba(255, 255, 255, 0.05)' 
+    c.fillRect(0, 0, innerWidth, innerHeight)
+    requestAnimationFrame(animate)
+    partcles.forEach(partcle => {
+        partcle.update() 
+    }); 
 }
 
 const init = () => {
-     enemy = new Enemy(100, 100, 1, 1, 20, colors[Math.floor(Math.random() * colors.length)])
-     player = new Player() 
+    let minDistance = 50; 
+     for(let i = 0; i < 10; i++){
+        minDistance = minDistance + 5
+        const randomColor = colors[Math.floor(Math.random() * colors.length)]
+        const partcle = new Particle(2, randomColor, minDistance, randomIntFromRange(0.04,0.06))
+         partcles.push(partcle)
+     }
     animate()
 } 
 init()
